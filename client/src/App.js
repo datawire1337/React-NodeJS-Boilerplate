197d45e572d4439fdb605a3da36db5db46c381ce
Kilian Macharis <info@datawire.be>
2021-03-26 13:34:30 +0100

    Initial commit: TRIXXO NN App

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios'
+
+class App extends Component {
+  state = {
+    response: {}
+  };
+  
+  componentDidMount() {
+    axios.get('/api/v1/say-something').then((res) => {
+      const response = res.data;
+      this.setState({response});
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello from the frontend!</h1>
+        <h1>{this.state.response.body}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
